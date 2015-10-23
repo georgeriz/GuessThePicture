33a06b2d6c69f2c0c72fd6bda3703e3b96142734
georgeriz <trivialgeorge@gmail.com>
2015-10-23 22:12:45 +0200

    Full screen while playing for version >=KITKAT

--- app/src/main/java/com/example/george/guessthepicture/GameActivity.java
+++ app/src/main/java/com/example/george/guessthepicture/GameActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.media.SoundPool;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Environment;
@@ -17,7 +18,7 @@ import android.widget.Toast;
 
 import java.io.File;
 
-public class GameActivity extends FragmentActivity implements TimeUpFragment.OnFragmentInteractionListener{
+public class GameActivity extends FragmentActivity implements TimeUpFragment.OnFragmentInteractionListener {
     final static String NUMBER_CORRECT = "com.example.george.guessthepicture.CORRECT";
     final static String NUMBER_TOTAL = "com.example.george.guessthepicture.TOTAL";
     /**
@@ -137,6 +138,22 @@ public class GameActivity extends FragmentActivity implements TimeUpFragment.OnF
         intent.putExtra(NUMBER_CORRECT, nCorrect);
         intent.putExtra(NUMBER_TOTAL, nTotal);
         startActivity(intent);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            if (hasFocus) {
+                getWindow().getDecorView().setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+            }
+        }
     }
 
     @Override
